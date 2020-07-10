fe5cb027f1aad913944d90868a74b4f3c3da6307
Starlotte <65743811+Starlotte@users.noreply.github.com>
2020-07-10 15:05:37 +0900

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,1 +1,16 @@
+<?php
 
+if (isset($_POST['submit'])) {
+   $name = $_POST['name'];
+   $subject = $_POST['subject'];
+   $mailFrom = $_POST['mail'];
+   $message = $_POST['message'];
+
+   $mailTo = "starlottemusic@gmail.com"
+   $headers = "COMMISSION: ".$mailFrom
+   $txt = "You have recieved a commission from ".$name.".\n\n".$message;
+
+   mail($mailTo, $subject, $headers);
+   header("Location: index.php?mailsent");
+
+}
